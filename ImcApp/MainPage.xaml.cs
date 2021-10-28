1cb2da08fe0335ed7cc211a512c41e8a94a1730d
David Alejandro Uc Dzib <17470037@campeche.tecnm.mx>
2021-10-27 19:52:08 -0500

    Agrega el manejo de eventos que da comportamiento a la interfaz de usuario de la aplicación v2

--- ImcApp/MainPage.xaml.cs
+++ ImcApp/MainPage.xaml.cs
@@ -20,6 +20,24 @@ namespace ImcApp
             //imcLabel.Text = string.Empty;
             //situacionNutricionalLabel.Text = string.Empty;
         }
+        private string GetEstadoNutricional(CalculadoraImc.EstadoNutricional estado)
+        {
+            switch (estado)
+            {
+                case CalculadoraImc.EstadoNutricional.PesoBajo:
+                    return "Peso bajo";
+                case CalculadoraImc.EstadoNutricional.PesoNormal:
+                    return "Peso normal";
+                case CalculadoraImc.EstadoNutricional.SobrePeso:
+                    return "Sobrepeso";
+                case CalculadoraImc.EstadoNutricional.Obesidad:
+                    return "Obesidad";
+                case CalculadoraImc.EstadoNutricional.ObesidadExtrema:
+                    return "Obesidad extrema";
+                default:
+                    return string.Empty;
+            }
+        }
 
         private void CalcularButton_Clicked(object sender, EventArgs e)
         {
@@ -31,7 +49,8 @@ namespace ImcApp
             {
                 CalculadoraImc cimc = new CalculadoraImc(peso, estatura);
                 imcLabel.Text = string.Format("{0:F4}", cimc.Imc);
-                situacionNutricionalLabel.Text = cimc.SituacionNutricional.ToString();
+                situacionNutricionalLabel.Text = GetEstadoNutricional(cimc.SituacionNutricional);
+                    //cimc.SituacionNutricional.ToString();
             }
             else
             {
